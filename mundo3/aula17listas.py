490280a07aac71bf6a433afc6ae240febdf46c8f
samantaProspero <samantaprospero@gmail.com>
2020-08-17 17:09:18 -0300

    aula18

--- mundo3/aula17listas.py
+++ mundo3/aula17listas.py
@@ -160,8 +160,18 @@ print(f'Os valores ímpares foram: {impares}')"""
 # 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
 # Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
-
-# exp = str(input('Digite uma expressão Matemática: ')).strip()
-# while True:
-
-
+"""exp = str(input('Digite uma expressão Matemática: ')).strip()
+pilha = []
+for simb in exp:
+    if simb == '(':
+        pilha.append(simb)
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Expressão correta.')
+else:
+    print('Expressão incorreta.')"""
